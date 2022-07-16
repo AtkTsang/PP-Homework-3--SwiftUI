9e0fbd216c5b0aa5f3cb348cc2a7745ffc9bcfa8
atktsang <atktsang@gmail.com>
2022-07-16 14:01:45 +0800

    Initial Commit

---
+++ PP Homework 3- SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PP Homework 3- SwiftUI
+//
+//  Created by Anthony Tsang on 16/7/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PP Homework 3- SwiftUI/PP_Homework_3__SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PP_Homework_3__SwiftUIApp.swift
+//  PP Homework 3- SwiftUI
+//
+//  Created by Anthony Tsang on 16/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct PP_Homework_3__SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
