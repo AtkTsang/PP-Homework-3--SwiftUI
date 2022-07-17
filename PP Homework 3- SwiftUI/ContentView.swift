5bbd8a6ad773d187d79d0c026288f32183432949
atktsang <atktsang@gmail.com>
2022-07-17 13:21:31 +0800

    PP homework 3 in SwiftUI

--- PP Homework 3- SwiftUI/ContentView.swift
+++ PP Homework 3- SwiftUI/ContentView.swift
@@ -7,15 +7,111 @@
 
 import SwiftUI
 
+
+let image = Image("Community")
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        NavigationView{
+            VStack (spacing: 0){
+                NavigationLink{
+                    RightPersonView()
+                } label: {
+                    VStack {
+                        Text("You are already accepted")
+                            .font(.title)
+                        .foregroundColor(.green)
+                        Image("Community/Greendale Flag")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:200, height: 200)
+                    }
+                }
+            }
+        }
+        .navigationTitle("那個人是你嗎？")
+    }
+}
+
+struct RightPersonView:View {
+    
+    @State private var currentPage = 0
+    
+    var body: some View {
+       
+        VStack {
+            TabView (selection: $currentPage) {
+                ForEach(1..<8){ item in
+                    VStack (spacing: 0){
+                        Image("numberCommunity2/\(item)")
+                        ScrollView {
+                            Text(charaDes[item-1])
+                                .padding()
+                        }
+                        Text("I am the \(currentPage+1) of Greendale Seven")
+                            .scaledToFit()
+                        Button {
+                           currentPage = (currentPage + 1) % 7
+                        } label: {
+                            Image(systemName: "arrow.right.circle.fill")
+                               .resizable()
+                               .frame(width: 40, height: 40)
+                        }
+                        .padding(.bottom)
+                    }
+                }
+            }
+            
+            .tabViewStyle(.page)
             .padding()
+            .indexViewStyle(.page(backgroundDisplayMode: .always))
+            
+
+            
+            
+        }
+        /*
+        VStack {
+            Image("Jeff")
+                .resizable()
+                .scaledToFit()
+                .navigationTitle("Jeff")
+                .navigationBarTitleDisplayMode(.inline)
+            NavigationLink{
+                
+            } label: {
+                Text("City College")
+            }
+            
+                
+        }
+        */
+    }
+}
+
+struct WrongPersonView:View {
+    var body: some View {
+        VStack {
+            Image("cityCollegeDean")
+                .resizable()
+                .scaledToFit()
+                .navigationTitle("City College")
+                .navigationBarTitleDisplayMode(.inline)
+        }
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct RightPersonView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            RightPersonView()
+        }
     }
 }
---
+++ PP Homework 3- SwiftUI/characterBio.swift
@@ -0,0 +1,18 @@
+//
+//  characterBio.swift
+//  PP Homework 3- SwiftUI
+//
+//  Created by Anthony Tsang on 17/7/2022.
+//
+
+import Foundation
+
+let charaDes = [
+                "Jeff is a charming but manipulative Amoral Attorney who was forced to attend Greendale upon the discovery that his college credentials were forged. He is the de facto leader of the study group, and much of the show focuses on his struggle between wanting to remain self-serving and lazy, but also wanting to help his new friends at Greendale.",
+                "Troy is a charismatic but not-incredibly-intelligent high-school football star who decided to attend Greendale after he suffered an injury that put a halt to him having a college football career. Despite seeming like a typical jock at first, he quickly starts to show his nerdy side after he becomes best friends with Abed due to their shared love of pop culture and goofy antics.",
+                "Abed is a media-aware and pop-culture-obsessed film student who is either suffering from an unspecific psychological disorder or is just very eccentric. He tends to view the world around him as if he and his friends were the main characters in a weekly television sitcom about a group of oddball students. As such, he tends to make frequent references, comparisons and 'homages' to various TV shows and movies.",
+                "Annie is an insecure and neurotic overachiever attending Greendale following an addiction to prescription medication which ruined her previously flawless high-school record. Intelligent and driven, she's usually perky and cheerful but tends to get tightly-wound and uptight when things fail to go to plan or when matters of grades or academic achievement arise.",
+                "Pierce is the oldest member of the study group; a local curmudgeon and a wealthy but lonely tycoon who attends Greendale primarily to find company and give himself something to do. Not nearly as smart or popular as he thinks, he tends to act inappropriately and thus come across as racist, sexist and buffoonish.",
+                "britta is a passionate but inept political activist, feminist, anarchist cat owner and psychology student, who, although generally well meaning, tends to eagerly latch on to particular causes with more enthusiasm (and obnoxious self-righteousness) than actual understanding or dedication.",
+                "Shirley is a divorced housewife and mother who started attending business classes at the school following her divorce. A devout Christian, she tends to see herself as the group's moral and spiritual compass and conscience—whether the group wants her to play that role or not. Her sugary-sweet demeanor barely hides thinly-veiled rage issues."
+]
